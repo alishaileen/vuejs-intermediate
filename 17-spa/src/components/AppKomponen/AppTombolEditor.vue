7b94961002abb73df798ebf002c7946fca18baab
Aileen <alishaileen05@gmail.com>
2020-10-16 16:33:56 +0700

    idk man where I'm on rn

---
+++ 17-spa/src/components/AppKomponen/AppTombolEditor.vue
@@ -0,0 +1,36 @@
+<!-- @format -->
+
+<template>
+  <b-button
+    :disabled="nonaktif"
+    class="margin-right"
+    outlined
+    :type="warna"
+    :icon-right="icon"
+    @click="ketikaTombolDiKlik"
+  />
+</template>
+
+<script>
+export default {
+  name: "AppTombolEditor",
+  props: {
+    nonaktif: {
+      type: Boolean,
+      default: false,
+    },
+    warna: {
+      type: String,
+      default: "is-dark",
+    },
+    icon: {
+      type: String,
+    },
+  },
+  methods: {
+    ketikaTombolDiKlik(event) {
+      this.$emit("klik", event);
+    },
+  },
+};
+</script>
---
+++ 17-spa/src/components/ListKode.vue
@@ -0,0 +1,123 @@
+<!-- @format -->
+
+<template>
+  <main>
+    <section id="list-kode" class="section is-small">
+      <div class="container">
+        <b-button class="button is-info" tag="router-link" to="/">
+          Kembali
+        </b-button>
+
+        <h3 v-show="$store.state.pengguna.idPengguna" class="title is-3">
+          Halo, {{ $store.state.pengguna.namaPengguna }}
+        </h3>
+
+        <app-bagian-opsi-daftar-kode
+          :banyak-data.sync="filter.banyakData"
+          :urutkan-berdasarkan.sync="filter.urutkanBerdasarkan"
+          :urutkan.sync="filter.urutkan"
+          :apakah-highlight-menyala.sync="filter.apakahHighlightMenyala"
+        />
+
+        <app-bagian-daftar-kode
+          :halaman.sync="filter.halaman"
+          :apakah-highlight-menyala="filter.apakahHighlightMenyala"
+          :dapatkan-daftar-kode="dapatkanDaftarKode"
+        />
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+// import { stringifyUrl } from "query-string";
+import debounce from "debounce-fn";
+// import cleanDeep from "clean-deep";
+
+import AppBagianOpsiDaftarKode from "./bagian/AppBagianOpsiDaftarKode";
+import AppBagianDaftarKode from "./bagian/AppBagianDaftarKode";
+
+// import AppTombol from "./AppKomponen/AppTombol";
+
+import { dapatkanOpsi } from "../utils";
+// import { URL_API, OPSI_STRINGIFY } from "../constants";
+
+export default {
+  name: "ListKode",
+  components: {
+    AppBagianOpsiDaftarKode,
+    AppBagianDaftarKode,
+
+    // AppTombol,
+  },
+  data() {
+    return {
+      filter: {
+        halaman: 1,
+        banyakData: this.$store.state.kode.daftarKode.length,
+        urutkanBerdasarkan: "createdAt",
+        urutkan: "DESC",
+        apakahHighlightMenyala: 1,
+      },
+      hasilHighlight: "",
+      daftarBahasaPemrograman: [],
+
+      isModalOpened: false,
+    };
+  },
+  watch: {
+    "$store.state.pengguna.idPengguna"(idPengguna) {
+      if (idPengguna) {
+        this.dapatkanDaftarKode();
+      }
+    },
+    filter: {
+      handler: debounce(
+        function () {
+          this.dapatkanDaftarKode();
+        },
+        { wait: 500 }
+      ),
+      deep: true,
+    },
+  },
+  created() {
+    if (!this.$store.state.pengguna.idPengguna) {
+      this.$router.push("/");
+    }
+  },
+  methods: {
+    async dapatkanDaftarKode() {
+      await this.$store.dispatch("kode/dapatkanSemuaKode", {
+        idPengguna: this.$store.state.pengguna.idPengguna,
+        filter: {
+          halaman: this.filter.halaman,
+          banyakData: this.filter.banyakData,
+          urutkanBerdasarkan: this.filter.urutkanBerdasarkan,
+          urutkan: this.filter.urutkan,
+          apakahHighlightMenyala: this.filter.apakahHighlightMenyala,
+        },
+      });
+    },
+    async dapatkanDaftarBahasaPemrograman() {
+      try {
+        const respon = await dapatkanOpsi();
+        if (respon.success && !respon.error) {
+          this.daftarBahasaPemrograman = respon.data.languages;
+        }
+      } catch (error) {
+        const dataNotifikasiGalat = {
+          apakahTampil: true,
+          pesan: error.message,
+          warnaPesan: "is-danger",
+        };
+        this.$store.dispatch(
+          "notifikasi/tampilkanNotifikasi",
+          dataNotifikasiGalat
+        );
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
